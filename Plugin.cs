4787f1c4892eb0450fb1bcf8cad5303419ffa5be
qstage <166386817+qstage@users.noreply.github.com>
2024-11-24 22:26:33 +0300

    init

---
+++ Plugin.cs
@@ -0,0 +1,35 @@
+﻿using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Memory;
+using CounterStrikeSharp.API.Modules.Memory.DynamicFunctions;
+
+namespace MultiboostFix;
+
+public class Plugin : BasePlugin
+{
+    public override string ModuleName => "MultiboostFix";
+    public override string ModuleVersion => "1.0.0";
+    public override string ModuleAuthor => "xstage";
+
+    public override void Load(bool hotReload) =>
+        VirtualFunctions.CCSPlayerPawnBase_PostThinkFunc.Hook(Hook_PostThink, HookMode.Post);
+
+    public override void Unload(bool hotReload) =>
+        VirtualFunctions.CCSPlayerPawnBase_PostThinkFunc.Unhook(Hook_PostThink, HookMode.Post);
+
+    private HookResult Hook_PostThink(DynamicHook hook)
+    {
+        var pawn = hook.GetParam<CCSPlayerPawn>(0);
+
+        if (pawn == null || !pawn.IsValid) return HookResult.Continue;
+
+        var groundEntity = pawn.GroundEntity;
+        var nextGroundEntity = groundEntity.Value?.As<CCSPlayerPawn>().GroundEntity;
+
+        if (groundEntity is { IsValid: true } && nextGroundEntity is { IsValid: true })
+        {
+            groundEntity.Raw = nextGroundEntity.Raw;
+        }
+        
+        return HookResult.Continue;
+    }
+}
